32c0b97d7083c374b8fd06521323facd6d44023e
Панов Микита <panov.mykyta@gmail.com>
2025-10-22 14:53:18 +0300

    added division

--- calculator.cpp
+++ calculator.cpp
@@ -8,8 +8,12 @@ int Calculator::Sub (double a, double b)
 {
     return Add (a, -b);
 }
-
 int Calculator::Mul (double a, double b)
 {
     return a * b + 0.5;
 }
+int Calculator::Div (double a, double b)
+{
+    return a / b + 0.5;
+}
+//I must be the reason why
